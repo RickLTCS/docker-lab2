3089cccf54ef4fa98ae7bd362664c8fc6b1a85ec
Patrick Anley <19517303@students.latrobe.edu.au>
2024-08-15 17:35:55 +1000

    added date

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 app.get('/', (req, res) => {
     child_process.exec('fortune', (error, message) => {
         if (error === null) {
-            res.send(message);
+            const currentDateTime = new Date().toString();
+            const responseMessage = 'Date and Time: ' + currentDateTime + '\nFortune:' + message;
+            res.send(responseMessage);
         } else {
             res.send('Error: ' + error);
         }
